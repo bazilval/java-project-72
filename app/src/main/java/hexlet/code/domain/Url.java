a7f74e161926efc40d0caace1bd4f7c2172dcfeb
Valentin Bazilevich <bazilvalentin@gmail.com>
2023-07-09 18:14:27 +0300

    Connected Ebean and in-memory db H2

---
+++ app/src/main/java/hexlet/code/domain/Url.java
@@ -0,0 +1,31 @@
+package hexlet.code.domain;
+
+import io.ebean.Model;
+import io.ebean.annotation.WhenCreated;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+public class Url extends Model {
+    @Id
+    private Long id;
+    private String name;
+    @WhenCreated
+    private Instant createdAt;
+
+    public Url() {
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+}
